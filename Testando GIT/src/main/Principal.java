4001d0dcb6892e64a401e401a1d02cbc7e1f7ca6
guisardapinheiro <guisardapinheiro@hotmail.com>
2013-06-28 13:34:28 -0300

    Update Principal.java
    teste pull

--- Testando GIT/src/main/Principal.java
+++ Testando GIT/src/main/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Testando GIT - modificado!");
-
+		System.out.println("apenas testando o pull");
 	}
 
 }
